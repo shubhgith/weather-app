78dca44667b8b9113b60d3a171797fb3be021ae4
shubhgith <shubhaggarwal23062000@gmail.com>
2020-11-23 03:58:52 +0530

    =added humidity in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,18 +3,18 @@ const request = require("request");
 
 const forecast =(address,callback) =>{
     const url = "http://api.weatherstack.com/current?access_key=5e67b7916725099355a56a45eff88b09&query="+address;
-    request({url,json:true},(error,response)=>{
+    request({url,json:true},(error,{body})=>{
         if(error){
             //console.log("Unable to connect to network!")
             callback("Unable to connect to network!")
         }
-        else if(response.body.error){
+        else if(body.error){
            // console.log("wrong location entered!")
             callback("wrong location entered!")
         }else{
            callback(undefined,
-            {location:response.body.location.name+","+response.body.location.region,
-            forecastData:"It is currently "+ response.body.current.temperature +" degrees out."+ " It feels like "+ response.body.current.feelslike +" degrees out"})
+            {location:body.location.name+","+body.location.region,
+            forecastData:body.current.weather_descriptions+"!"+"\n"+ "It is currently "+body.current.temperature +" degrees out."+ " It feels like "+ body.current.feelslike +" degrees out"+" \n Humidity:"+body.current.humidity+"%."})
         }
     })
 }
